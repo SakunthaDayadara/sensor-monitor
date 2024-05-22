e8de16444993354118b29bc076306abe4e498fd0
sadee1010 <117168733+sadee1010@users.noreply.github.com>
2024-05-22 14:45:15 +0530

    User model & controller

---
+++ backend/app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_many :sensor, dependent: :destroy
+    before_create :generate_user_id
+  
+    self.primary_key = 'user_id'
+  
+    private
+  
+      def generate_user_id
+        last_user = User.last
+        if last_user.nil?
+          self.user_id = 'US0001'
+        else
+          last_id = last_user.user_id[2..-1].to_i
+          new_id = last_id + 1
+          self.user_id = "US#{'%04d' % new_id}"
+        end
+      end
+  
+  
+  end
+  
