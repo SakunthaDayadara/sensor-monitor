6e5b28a54b6064f33da6e297ede5a73f1191bcf3
Sakuntha <sakunthadayadara2001@gmail.com>
2024-06-02 18:15:30 +0530

    Token Decode

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::API
-end
+    before_action :authorized
+  
+    def authorized
+      header = request.headers['Authorization']
+      token = header.split(' ').last if header
+      if token
+        begin
+          @decoded = JsonWebToken.decode(token)
+          @current_user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+          render json: { errors: 'User not found' }, status: :unauthorized
+        rescue JWT::DecodeError => e
+          render json: { errors: 'Invalid token' }, status: :unauthorized
+        end
+      else
+        render json: { errors: 'Token not provided' }, status: :unauthorized
+      end
+    end
+  
+    def current_user
+      @current_user
+    end
+  
+    def require_authorization
+      render json: { errors: 'Not authorized' }, status: :unauthorized unless current_user
+    end
+  
+  
+  
+  end
+  
